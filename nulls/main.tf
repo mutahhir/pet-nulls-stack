b805e001d69c13ce66f504a5f70e805e8ab2b761
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2024-04-02 15:26:40 +0200

    testing

--- nulls/main.tf
+++ nulls/main.tf
@@ -26,12 +26,18 @@ resource "null_resource" "this" {
   }
 }
 
-resource "null_resource" "dns_check" {
-  count = 1
+# resource "null_resource" "dns_check" {
+#   count = 1
+#
+#   provisioner "local-exec" {
+#     command     = "false"
+#     interpreter = ["bash", "-c"]
+#   }
+# }
 
-  provisioner "local-exec" {
-    command     = "false"
-    interpreter = ["bash", "-c"]
+resource "null_resource" "errorplz" {
+  triggers = {
+    pet = 1
   }
 }
 
@@ -40,6 +46,10 @@ output "ids" {
   value = [for n in null_resource.this: n.id]
 }
 
+output "exit" {
+  value = jsondecode(null_resource.errorplz.id)
+}
+
 output "name" {
   value = local.name
-  }
+}
